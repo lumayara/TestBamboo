0d75cc4d27ef8e788e1c264066d60c44c542cfdd
Luana Mayara Fernandes da Silva Bezerra <lumayara.ads@gmail.com>
2017-10-26 15:11:43 -0400

    HelloWorld
    Hello World with unit testing

---
+++ TestHelloWorld/TestHelloWorld.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestHelloWorld.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HelloWorld hello = new HelloWorld();
+            Assert.AreEqual(hello.Name, "Hello LCG Team");
+        }
+    }
+}
---
+++ TestHelloWorld/TestHelloWorld/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestHelloWorld
+{
+    public class HelloWorld
+    {
+        public String Name;
+
+        public HelloWorld()
+        {
+            Name = "Hello LCG Team";
+        }
+    }
+}
